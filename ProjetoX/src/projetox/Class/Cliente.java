7a9ad8c1c01e2799a8baa67dde94586a17277101
Matheus Andrade <Matheus Andrade@172.16.205.168>
2019-11-21 21:14:38 -0200

    Adição Olá Mundo

--- ProjetoX/src/projetox/Class/Cliente.java
+++ ProjetoX/src/projetox/Class/Cliente.java
@@ -11,4 +11,6 @@ package projetox.Class;
  */
 public class Cliente {
     
+    
+    
 }
--- ProjetoX/src/projetox/ProjetoX.java
+++ ProjetoX/src/projetox/ProjetoX.java
@@ -15,6 +15,8 @@ public class ProjetoX {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("Olá Mundo");
         // TODO code application logic here
     }
     
