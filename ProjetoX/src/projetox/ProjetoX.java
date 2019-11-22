1a9af1626bb45c21a65fe75a09ecc41062978a6a
Matheus Andrade <Matheus Andrade@10.246.246.94>
2019-11-21 22:31:12 -0200

    HEHE

--- ProjetoX/src/projetox/ProjetoX.java
+++ ProjetoX/src/projetox/ProjetoX.java
@@ -15,7 +15,7 @@ public class ProjetoX {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Freitas é viado");
         
         // TODO code application logic here
     }
