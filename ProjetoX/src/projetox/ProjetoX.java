fa9bc060748156b1ab1284d793fca7b5f71e0ff5
Matheus Andrade <Matheus Andrade@10.246.246.94>
2019-11-21 22:24:33 -0200

    Teste

--- ProjetoX/src/projetox/ProjetoX.java
+++ ProjetoX/src/projetox/ProjetoX.java
@@ -17,6 +17,7 @@ public class ProjetoX {
     public static void main(String[] args) {
         
         System.out.println("Hello World");
+        System.out.println("Teste");
         
         // TODO code application logic here
     }
