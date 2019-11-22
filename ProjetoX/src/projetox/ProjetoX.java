351179b81680d97e531a427577c22f2ea77714f1
Matheus Andrade <Matheus Andrade@10.246.246.94>
2019-11-21 22:22:24 -0200

    Hello World

--- ProjetoX/src/projetox/ProjetoX.java
+++ ProjetoX/src/projetox/ProjetoX.java
@@ -16,6 +16,8 @@ public class ProjetoX {
      */
     public static void main(String[] args) {
         
+        System.out.println("Hello World");
+        
         // TODO code application logic here
     }
     
