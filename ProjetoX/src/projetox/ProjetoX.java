71baeb15f4a140b743b9535a5c313bebe99ccd89
Matheus Andrade <Matheus Andrade@10.246.246.94>
2019-11-21 22:30:06 -0200

    Apagando SOUT

--- ProjetoX/src/projetox/ProjetoX.java
+++ ProjetoX/src/projetox/ProjetoX.java
@@ -16,9 +16,6 @@ public class ProjetoX {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hello World");
-        System.out.println("Teste");
-        System.out.println("Teste2");
         
         // TODO code application logic here
     }
