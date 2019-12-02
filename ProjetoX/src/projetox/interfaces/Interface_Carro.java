b03718a78a0d2a47b148ef35935d7453b5db9cf0
Matheus Freitas <Matheus Freitas@192.168.1.106>
2019-12-01 21:42:18 -0300

    Interface de carro e funcionario

--- ProjetoX/src/projetox/interfaces/Interface_Carro.java
+++ ProjetoX/src/projetox/interfaces/Interface_Carro.java
@@ -5,10 +5,26 @@
  */
 package projetox.interfaces;
 
+import java.util.ArrayList;
+import projetox.Class.Carro;
+
 /**
  *
  * @author Luan Paulo
  */
 public interface Interface_Carro {
+    
+    public String Cadastrar_Carro(Carro novo);
+    
+    public ArrayList<Carro> buscar_Carro(String modelo) throws Exception;
+    
+    public String remover_Carro(String Modelo)throws Exception;
+    
+    public void atualizar_Carro(Carro novo,String Modelo)throws Exception;
+    
+    public boolean veirificarRemocao(String cargo)throws Exception;
+    
+    public boolean veirificarAtualizacao()throws Exception;
+    
     
 }
--- ProjetoX/src/projetox/interfaces/Interface_Funcionario.java
+++ ProjetoX/src/projetox/interfaces/Interface_Funcionario.java
@@ -5,10 +5,29 @@
  */
 package projetox.interfaces;
 
+import java.util.ArrayList;
+import projetox.Class.Funcionario;
+
 /**
  *
  * @author Luan Paulo
  */
 public interface Interface_Funcionario {
+    
+    public String Cadastrar_Funcionario(Funcionario novo);
+    
+    public ArrayList<Funcionario> buscar_Funcionario(String nome) throws Exception;
+    
+    public String remover_Funcionario(int id)throws Exception;
+    
+    public void atualizar_Funcionario(Funcionario novo,int id)throws Exception;
+    
+    public boolean veirificarRemocao(String cargo)throws Exception;
+    
+    public boolean veirificarAtualizacao()throws Exception;
+    
+    public Funcionario VerificarLogin(String login,String senha) throws Exception;
+    
+    public boolean verificarLoginDisponivel(String login) throws Exception;
     
 }
